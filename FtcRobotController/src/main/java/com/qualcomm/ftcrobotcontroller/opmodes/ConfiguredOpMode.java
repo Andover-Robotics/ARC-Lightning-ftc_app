2295e30bf459b133ff2cf103fff84db63c47b847
Chairman Zhao <chairmanzhao42@gmail.com>
2016-02-23 15:52:53 -0500

    2_23_16

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ConfiguredOpMode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ConfiguredOpMode.java
@@ -11,18 +11,21 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by Chairman Zhao on 1/18/2016.
  */
 public abstract class ConfiguredOpMode extends LinearOpMode {
-    protected DcMotor dLeft, dRight, sLeft, sRight, sweep;                //The 'd' prefix indicates a drivetrain component, the 's' prefix indicates a slide component
+    protected DcMotor dLeft, dRight, dL2, dR2, sLeft, sRight, sweep;                //The 'd' prefix indicates a drivetrain component, the 's' prefix indicates a slide component
     protected Servo bRack, bTilt, hLeft, hRight;                          /*The 'b' prefix indicates a bucket component, the 'h' prefix indicates a hanger-manipulator
                                                                           bRack = Rack and Pinion, bTilt = Servo for tilting bucket*/
     protected DcMotor[] linSlide;
     //protected GyroSensor gyro;
     //private final float JOY_PWR_MULT = (float) 0.8;
+    public final double WHEEL_DIAMETER = 2.75, TICKS_PER_INCH = 1440/WHEEL_DIAMETER;
     protected void stopRobot(){
         dLeft.setPower(0);
         dRight.setPower(0);
         sLeft.setPower(0);
         sRight.setPower(0);
         sweep.setPower(0);
+        dL2.setPower(0);
+        dR2.setPower(0);
     }
     public void reverseMotor(DcMotor motor){
         if(!(motor.getDirection() == DcMotor.Direction.REVERSE))
@@ -32,6 +35,8 @@ public abstract class ConfiguredOpMode extends LinearOpMode {
         dLeft = hardwareMap.dcMotor.get("DL");
         dRight = hardwareMap.dcMotor.get("DR");
         sLeft = hardwareMap.dcMotor.get("SL");
+        dL2 = hardwareMap.dcMotor.get("FL");
+        dR2 = hardwareMap.dcMotor.get("FR");
         reverseMotor(sLeft);
         reverseMotor(dRight);
         sRight = hardwareMap.dcMotor.get("SR");
